8533a68724245917d70224bd45026d4f9407d370
Steven A White <sawhite@ara.com>
2023-08-08 03:55:00 -0400

    Improved variable handling for Fetch Content to reduce cache noise

--- cmake/FindTinyXML2.cmake
+++ cmake/FindTinyXML2.cmake
@@ -53,17 +53,33 @@ if(TinyXML2_ROOT_DIR)
 endif()
 
 # Find headers and libraries
+
 find_path(TinyXML2_INCLUDE_DIR NAMES tinyxml2.h PATH_SUFFIXES "tinyxml2" ${TinyXML2_INCLUDE_PATH})
+
+find_library(TinyXML2_LIBRARY_RELEASE NAMES tinyxml2 )
+find_library(TinyXML2_LIBRARY_DEBUG NAMES tinyxml2d tinyxml2_d tinyxml2)
 
 mark_as_advanced(TinyXML2_INCLUDE_DIR)
 
 # Output variables generation
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(TinyXML2 DEFAULT_MSG TinyXML2_INCLUDE_DIR)
-
-set(TinyXML2_FOUND ${TINYXML2_FOUND})
-unset(TINYXML2_FOUND)
+find_package_handle_standard_args(TinyXML2 DEFAULT_MSG 
+                                  TinyXML2_INCLUDE_DIR 
+                                  TinyXML2_LIBRARY_RELEASE 
+                                  TinyXML2_LIBRARY_DEBUG
+)
 
 if(TinyXML2_FOUND)
     set(TinyXML2_INCLUDE_DIRS ${TinyXML2_INCLUDE_DIR})
+    if(NOT TARGET tinyxml2::tinyxml2)
+          add_library(tinyxml2::tinyxml2 INTERFACE IMPORTED GLOBAL)
+          
+          set_target_properties(tinyxml2::tinyxml2
+          PROPERTIES
+          INTERFACE_INCLUDE_DIRECTORIES
+          ${TinyXML2_INCLUDE_DIR}
+          )
+          target_include_directories(tinyxml2::tinyxml2 INTERFACE $<INSTALL_INTERFACE:include> INTERFACE $<BUILD_INTERFACE:${TinyXML2_INCLUDE_DIR}> $<BUILD_INTERFACE:${TINYXML2_INCLUDE_DIR}>)
+          target_link_libraries(tinyxml2::tinyxml2 INTERFACE $<$<CONFIG:Debug>:${TinyXML2_LIBRARY_DEBUG}> INTERFACE $<$<CONFIG:>:${TinyXML2_LIBRARY_RELEASE}> INTERFACE $<$<CONFIG:Release>:${TinyXML2_LIBRARY_RELEASE}>)
+    endif()
 endif()
